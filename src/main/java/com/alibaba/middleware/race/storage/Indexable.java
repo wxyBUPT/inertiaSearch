683ed4c0a83990ba4b6c7bd2208238c75a13814b
wxyBUPT <746451950@qq.com>
2016-08-13 19:41:41 +0800

    返回disklock 的接口

--- src/main/java/com/alibaba/middleware/race/storage/Indexable.java
+++ src/main/java/com/alibaba/middleware/race/storage/Indexable.java
@@ -2,7 +2,7 @@ package com.alibaba.middleware.race.storage;
 
 /**
  * Created by xiyuanbupt on 7/21/16.
- * 的返回一个DiskLoc
+ * 一个实现返回disklock 的接口
  */
 public interface Indexable {
     DiskLoc getDataDiskLoc();
