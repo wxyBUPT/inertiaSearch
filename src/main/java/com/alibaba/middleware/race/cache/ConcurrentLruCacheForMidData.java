68e4230182b543e54d9a2322b524f8a9514c1893
wxyBUPT <746451950@qq.com>
2016-08-13 18:20:19 +0800

    一个有优先级的优先队列（优先级高的元素在队列前面，优先级低的元素一定在高优先级的元素后面）

--- src/main/java/com/alibaba/middleware/race/cache/ConcurrentLruCacheForMidData.java
+++ src/main/java/com/alibaba/middleware/race/cache/ConcurrentLruCacheForMidData.java
@@ -4,6 +4,7 @@ import com.alibaba.middleware.race.storage.IndexNode;
 
 /**
  * Created by xiyuanbupt on 7/24/16.
+ * 一个有优先级的队列(本项目中,非叶子节点的优先级要比叶子节点的优先级高,故在队列的前面)
  */
 public class ConcurrentLruCacheForMidData<KEY,VALUE extends IndexNode> extends ConcurrentLruCache<KEY,VALUE>{
 
